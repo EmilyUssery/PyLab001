b2d82aeb51262aed9fa3a116b03d048036e69ece
eussery122 <eussery122@mucr.redwoods.edu>
2025-09-02 13:14:39 -0700

    lab1 done

---
+++ lab1.py
@@ -0,0 +1,29 @@
+import math
+
+r=5
+A=math.pi * r**2
+print(A)
+
+V=4/3
+pi = math.pi * r**3
+print(pi)
+
+a = 3
+b = 4
+c = math.sqrt(a**2 + b**2)
+print (c)
+
+len("Emily Ussery")
+print("Emily Ussery")
+
+weight = 140
+height = 5*7
+bmi = weight/height**2
+print(bmi * 703)
+
+
+
+
+
+
+
